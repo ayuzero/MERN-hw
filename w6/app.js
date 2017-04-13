9614b3760b122cdd12cc083769954d3ddaebc707
ayuzero <ayuzero8427@gmail.com>
2017-04-13 19:09:07 +0800

    w7 commit

--- w6/app.js
+++ w6/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var port = process.env.PORT || 5000;
 
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 app.use('/css',express.static(__dirname + '/css_public'));
@@ -10,6 +12,9 @@ app.use('/img',express.static(__dirname + '/img_public'));
 app.use('/js',express.static(__dirname + '/js_public'));
 
 app.get('/', function( req , res ){
+    res.render('index');
+});
+app.get('/index', function( req , res ){
     res.render('index');
 });
 app.get('/about', function( req , res ){
